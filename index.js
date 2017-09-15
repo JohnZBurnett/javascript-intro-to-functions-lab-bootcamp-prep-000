c2475d0cabbb89b257390016f818b6644e64cf32
John Burnett <tuninator@gmail.com>
2017-09-15 13:23:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ function sayHiToGrandma(string) {
   if (string.toUpperCase === string) {
     return "YES INDEED!"
 
+  }
+
   else if (string === "I love you, Grandma.") {
     return "I love you, too."
   }
